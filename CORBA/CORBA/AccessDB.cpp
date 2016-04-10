41e85546808b9d35ddfc225e68e62dd07116449c
Sinholueiro <sinholueiro@hotmail.com>
2016-04-11 01:39:00 +0200

    DBFunctions

---
+++ CORBA/CORBA/AccessDB.cpp
@@ -0,0 +1,87 @@
+#include "mysql_connection.h"
+#include "mysql_driver.h"
+#include "mysql_error.h"
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/exception.h>
+#include <string.h>
+
+sql::Connection* getConexion() {
+	sql::mysql::MySQL_Driver *driver;
+	sql::Connection *connection;
+
+	driver = sql::mysql::get_mysql_driver_instance();
+	connection = driver->connect("tcp://localhost:3306/PDB", "lueiro", "0612");
+
+	return connection;
+}
+
+void setUser(sql::Connection* conexion, std::string nombre, std::string correo, std::string pass) {
+	sql::Statement *statement;
+	statement = conexion->createStatement();
+
+	std::string aux;
+	aux = "INSERT INTO usuarios VALUES('" + nombre + "','" + correo + "','" + pass + "');";
+	try {
+		statement->executeQuery(aux);
+	}catch (sql::SQLException &e) {}
+}
+
+int getUser(sql::Connection* conexion, std::string nombre) {
+	sql::Statement *statement;
+	sql::ResultSet *resultset;
+
+	statement = conexion->createStatement();
+
+	std::string aux;
+	aux = "SELECT * FROM usuarios WHERE nombre='" + nombre + "';";
+	resultset = statement->executeQuery(aux);
+
+	while (resultset->next()) {
+		return 1;
+	}
+	return 0;
+}
+
+void modPass(sql::Connection* conexion, std::string nombre, std::string pass) {
+	sql::Statement *statement;
+	statement = conexion->createStatement();
+
+	std::string aux;
+	aux = "UPDATE usuarios SET pass='" + pass + "' WHERE nombre='" + nombre + "';";
+	try {
+		statement->executeQuery(aux);
+	}
+	catch (sql::SQLException &e) {}
+}
+
+std::string* getAmigos(sql::Connection* conexion, std::string correo) {
+	sql::Statement *statement;
+	sql::ResultSet *resultset;
+
+	statement = conexion->createStatement();
+
+	std::string aux;
+	int size=1;
+	aux = "SELECT COUNT(correo2) FROM amigos WHERE correo1='" + correo + "';";
+	resultset = statement->executeQuery(aux);
+
+	if (resultset->next()) {
+		size=resultset->getInt(1);
+	}else {
+		std::string amigo[1];
+		amigo[0] = "Null";
+		return amigo;
+	}
+
+	std::string amigos[size];
+	aux = "SELECT correo2 FROM amigos WHERE correo1='" + correo + "';";
+	resultset = statement->executeQuery(aux);
+
+	int i = 0;
+	for (i = 0; resultset->next(); i++) {
+		std::string correo2 = resultset->getString(1);
+		amigos[i] = correo2;
+	}
+	return amigos;
+}
--- CORBA/CORBA/DB.cpp
+++ CORBA/CORBA/DB.cpp
@@ -1,27 +1,23 @@
-#include <mysql_connection.h>
-#include <cppconn/driver.h>
+#include "mysql_connection.h"
+#include "mysql_driver.h"
+#include "mysql_error.h"
 #include <cppconn/resultset.h>
 #include <cppconn/statement.h>
 #include <cppconn/exception.h>
 
-#pragma comment(lib, "mysqlcppconn.lib")
-
 void main(){
-	sql::Driver *driver;
+	sql::mysql::MySQL_Driver *driver;
 	sql::Connection *connection;
 	sql::Statement *statement;
 	sql::ResultSet *resultset;
 
 	//Conexión a MySQL
-	driver = get_driver_instance();
-	connection = driver->connect("tcp://127.0.0.1:3306", "lueiro", "0612");
-
-	//Use PDB;
-	connection->setSchema("PDB");
+	driver = sql::mysql::get_mysql_driver_instance();
+	connection = driver->connect("tcp://localhost:3306/PDB", "lueiro", "0612");
 
 	//Consulta
 	statement = connection->createStatement();
-	resultset = statement->executeQuery("SELECT * FROM usuarios");
+	resultset = statement->executeQuery("SELECT * FROM usuarios;");
 
 	//Print
 	while (resultset->next()) {
@@ -29,14 +25,15 @@ void main(){
 		std::cout << "\n Nombre: " << resultset->getString(2);
 		std::cout << std::endl;
 	}
-
 	// Edit (La consulta lanza una excepción con un error tipo 0 (éxito))
 	try {
-		statement->executeQuery("UPDATE usuarios SET correo=primero@correo.es WHERE correo=primero@correo.com");
+		statement->executeQuery("UPDATE usuarios SET correo='primero@correo.com' WHERE correo='primero@correo.es';");
 	}catch (sql::SQLException &e) {}
 
 	//Frees
 	delete resultset;
 	delete statement;
 	delete connection;
+
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
